9a3e968b893b96ec511c18d6d1e12a6fad1db3d1
Scott D. Peckham <Scott.Peckham@colorado.edu>
2022-11-01 12:31:25 -0600

    Add files via upload

--- lstm/__init__.py
+++ lstm/__init__.py
@@ -1,8 +1,6 @@
-# Without line below, get error message from NextGen:
-# NGen Framework 0.1.0
-# Building Nexus collection
-# Building Catchment collection
-# libc++abi.dylib: terminating with uncaught exception of type std::runtime_error: AttributeError: module 'lstm' has no attribute 'bmi_lstm'
-# zsh: abort      ./cmake_build/ngen ./data/catchment_data_lstm_test.geojson "cat-67"  "nex-65"
+# This is not necessary and may cause confusion.
+# If class name (bmi_LSTM) were to be exactly the same
+# as module name (bmi_lstm), this would cause trouble
+# because the module itself would no longer be accessible.
 
-from .bmi_lstm import bmi_LSTM
+# from .bmi_lstm import bmi_LSTM
