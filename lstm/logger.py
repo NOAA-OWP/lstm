c54d4f4f07d17a0161b0fa2f63c92054c4adf86c
Austin Raney <aaraney@protonmail.com>
2025-02-04 17:56:33 -0500

    feat: add logging module

---
+++ lstm/logger.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import logging
+import logging.config
+
+logger = logging.getLogger("bmi.lstm")
+
+
+def configure_logging(level: logging._Level = logging.INFO):
+    logging.config.dictConfig(logging_config)
+    logger.setLevel(level)
+
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "incremental": False,
+    "formatters": {
+        "short": {
+            "format": "[%(levelname)s|%(name)s|%(asctime)s]: %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+        },
+        "verbose": {
+            "format": "[%(levelname)s|%(name)s|%(asctime)s|%(module)s.%(funcName)s:%(lineno)d]: %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+        },
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "short",
+        },
+        "stderr": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+            "formatter": "verbose",
+            "level": "WARNING",
+        },
+    },
+    "loggers": {
+        "root": {
+            "handlers": ["stdout", "stderr"],
+            "level": "INFO",
+            "propagate": False,
+        }
+    },
+}
