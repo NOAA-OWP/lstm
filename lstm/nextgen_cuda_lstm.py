05b34400f400027262094c8fb188150dc67f066c
Austin Raney <aaraney@protonmail.com>
2025-02-04 18:03:17 -0500

    chore: remove dead imports; reformat

--- lstm/nextgen_cuda_lstm.py
+++ lstm/nextgen_cuda_lstm.py
@@ -1,18 +1,21 @@
 # LSTM here is based on PyTorch
-import torch
 from torch import nn
-#--------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------
 # This is the LSTM model. Based on the simple "CudaLSTM" in NeuralHydrology
 # Only meant for forward predictions, this is not for training. Do training in NeuralHydrology
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 class Nextgen_CudaLSTM(nn.Module):
-    def __init__(self, input_size, hidden_layer_size, output_size, batch_size, seq_length):
+    def __init__(
+        self, input_size, hidden_layer_size, output_size, batch_size, seq_length
+    ):
         super(Nextgen_CudaLSTM, self).__init__()
         self.input_size = input_size
         self.hidden_layer_size = hidden_layer_size
         self.seq_length = seq_length
         self.output_size = output_size
-        self.batch_size = batch_size # We shouldn't neeed to do a higher batch size.
+        self.batch_size = batch_size  # We shouldn't neeed to do a higher batch size.
         self.lstm = nn.LSTM(self.input_size, self.hidden_layer_size)
         self.head = nn.Linear(self.hidden_layer_size, self.output_size)
 
@@ -21,6 +24,6 @@ class Nextgen_CudaLSTM(nn.Module):
         c_t = c_t.float()
         input_layer = input_layer.float()
         input_view = input_layer.view(self.seq_length, self.batch_size, self.input_size)
-        output, (h_t, c_t) = self.lstm(input_view, (h_t,c_t))
+        output, (h_t, c_t) = self.lstm(input_view, (h_t, c_t))
         prediction = self.head(output)
         return prediction, h_t, c_t
