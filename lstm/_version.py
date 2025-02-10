3351832716bbdd65606f8a303e8f7df273f47f1d
Austin Raney <aaraney@protonmail.com>
2025-02-10 15:36:15 -0500

    tmp: update author; will squash

--- lstm/_version.py
+++ lstm/_version.py
@@ -1,6 +1,8 @@
 #
 # Copyright (C) 2025 Austin Raney, Lynker
 #
-# Author: Austin Raney <araney@lynker.com>
+# Author:
+#   Austin Raney <araney@lynker.com>
+#   Jonathan Frame <jmframe@ua.edu>
 #
 __version__ = "1.1.0"
